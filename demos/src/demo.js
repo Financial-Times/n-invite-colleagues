713b6d8ed506fa3377d392b310f123f2f9564fd1
Courtney.Osborn <courtney.osborn@FT-MW4996.local>
2018-02-07 08:50:05 +0000

    Removes banner  🐿 v2.7.0

--- demos/src/demo.js
+++ demos/src/demo.js
@@ -3,7 +3,6 @@ import oTracking from 'o-tracking';
 import inviteColleagues from '../../main';
 
 inviteColleagues.initEmbedded();
-inviteColleagues.initBanner();
 
 document.body.classList.add('js', 'enhanced');
 
