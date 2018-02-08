36c45fde51dbffe9d392903c37ac26b7b0f2856a
Courtney.Osborn <courtney.osborn@FT-MW4996.local>
2018-02-08 15:03:48 +0000

    removes test file  🐿 v2.7.0

--- test/setup.js
+++
@@ -1,6 +0,0 @@
-require('babel-register')({ // do I need this?
-	plugins: [
-		require.resolve('babel-plugin-add-module-exports'),
-		require.resolve('babel-plugin-transform-es2015-modules-commonjs')
-	]
-});
